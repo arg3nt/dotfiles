c2e9b4040cff7ab5d29991bcbe5468fc9aa34635
Nicholas Chera <nac@chera.xyz>
2024-07-17 02:04:43 -0700

    Echo newline string properly

--- .setup/dotfile_setup.sh
+++ .setup/dotfile_setup.sh
@@ -23,7 +23,7 @@ chsh -s $(which zsh) $(whoami)
 
 # Setup fbterm
 
-echo "#!/usr/bin/sh\nfbterm -- /usr/bin/sh -c \"TERM=fbterm login -p\"" | sudo tee /usr/sbin/fbterm-login
+echo -e "#!/usr/bin/sh\nfbterm -- /usr/bin/sh -c \"TERM=fbterm login -p\"" | sudo tee /usr/sbin/fbterm-login
 
 sudo sed -i 's/^ExecStart=-\/sbin\/agetty -o .*\$TERM$/ExecStart=-\/sbin\/agetty --noclear -n -l \/usr\/sbin\/fbterm-login %I \$TERM/g' \
     /usr/lib/systemd/system/getty@.service
